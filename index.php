fa996b1986193a752202d06da4733fd9cc88d894
Konya Donat <konya.donat@gmail.com>
2021-06-10 22:26:07 +0200

    included elements

--- index.php
+++ index.php
@@ -1,17 +1,23 @@
+<?= require_once './protected/config/config.php'; ?>
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
-        <meta charset="UTF-8">
-        <title></title>
+        <meta charset="<?=CHARSET ?>">
+        <title><?=Title ?></title>
+        
+        <?php if(!empty(CSS)){ 
+           $css_Item_Count = count(CSS); 
+           
+           for($i = 0;$i < $css_Item_Count;$i++){
+               echo '<link rel="stylesheet" type="text/css" href="'.CSS_DIR.CSS[$i].'">';
+           }
+        }?>
+        
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <?php include VIEWS_DIR.'header.php'; ?> 
+        <?php require PROTECTED_DIR.'menu.php';?>
+        <?php require PROTECTED_DIR.'content.php';?>
+        <?php include VIEWS_DIR.'footer.php';?> 
     </body>
 </html>
