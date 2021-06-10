a923ee651318afe7557387e7a02bb1b992e4548d
Konya Donat <konya.donat@gmail.com>
2021-06-10 23:55:34 +0200

    database managered done

--- protected/core/database_manager.php
+++ protected/core/database_manager.php
@@ -1,8 +1,45 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+function get_connection(){
+    $dsn = DB_TYPE.':dbname='.DB_NAME.';host='.DB_HOST;
+    $user = DB_USER;
+    $password = DB_PASS;
+    
+    $connection = new PDO($dsn,$user,$password);
+    $connection ->exec("SET NAMES 'utf8'");
+    return $connection;
+}
 
+function select($query,$row_base = false, $params =[]){
+    $connection = get_connection();
+    $statement = $connection->prepare($query);
+    $succes = $statement ->execute($params);
+    
+    $result = [];
+    if($succes==true){
+        if($row_base===false){
+            $result = $statement ->fetchAll();
+        }
+        else{
+            $result = $statement ->fetch();
+        }
+    }
+    else{
+        die('Sikertelen végrehajtás!');
+    }
+    
+    $statement -> closeCursor();
+    $connection = null;
+    
+    return $result;
+}
+function delete($query, $params = []){
+    $connection = get_connection();
+    $statement = $connection->prepare($query);
+    $succes = $statement ->execute($params);
+    
+    $statement -> closeCursor();
+    $connection = null;
+    
+    return $succes;
+}
