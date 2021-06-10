0416bd572efe1edbb7f8d0254e54683e22fd07b7
Konya Donat <konya.donat@gmail.com>
2021-06-10 21:48:48 +0200

    config updated

--- protected/config/config.php
+++ protected/config/config.php
@@ -2,4 +2,18 @@
 
 define('CHARSET','utf-8');
 define('Title', 'Webprog 2 beadandó');
-deinfe('CSS',['bootstarp.css']);
+define('CSS',['bootstarp.css']);
+
+
+define('ROOT_DIR','./');
+define('PROTECTED_DIR',ROOT_DIR.'protected/');
+define('PUBLIC_DIR',ROOT_DIR.'public/');
+define('VIEWS_DIR',PROTECTED_DIR.'views/');
+define('CSS_DIR',PUBLIC_DIR.'css/');
+define('CORE_DIR',PROTECTED_DIR.'core/');
+define('CONTENT_DIR',PROTECTED_DIR.'content/');
+define('FILES_DIR',PROTECTED_DIR.'files/');
+
+define('START_CONTENT','main');
+
+define('BASE_URL','http://localhost/OA4R7U-NBT_IM743G2-D/');
