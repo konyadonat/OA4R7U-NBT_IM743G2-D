c19b69e1703a9b406982c140304dbd58143ec254
Konya Donat <konya.donat@gmail.com>
2021-06-10 23:15:06 +0200

    created menu structure

--- protected/menu.php
+++ protected/menu.php
@@ -1,1 +1,29 @@
-<h2>Menu</h2>
+<?php
+
+$menu = [
+            [
+               'href' => '#',
+                'title' => 'Íróink',
+                'extra' => [],
+                'childs' => 
+                [
+                    [
+                        'href' => BASE_URL.'index.php?E=irok&M=lista',
+                        'title' => 'Íróink listázása',
+                        'extra' => []
+                    ],
+                    [
+                        'href' => BASE_URL.'index.php?E=irok&M=uj',
+                        'title' => 'Író felvétele',
+                        'extra' => ['target' => '_blank']
+                    ]
+                ]
+                
+            ]
+    
+];
+
+require_once CORE_DIR.'views.php';
+print_menu($menu);
+?>
+</br>
