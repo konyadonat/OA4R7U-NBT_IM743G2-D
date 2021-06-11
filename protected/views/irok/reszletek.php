bad59dab8c7b56f768472ff2eb80f3909a178de9
Konya Donat <konya.donat@gmail.com>
2021-06-11 19:11:44 +0200

    file formatted

--- protected/views/irok/reszletek.php
+++ protected/views/irok/reszletek.php
@@ -1,8 +1,10 @@
 <div class="container">
-<h3>Író neve</h3>
-<p><?=$result['nev']?></p>
-<h3>Születési éve</h3>
-<p><?=$result['szuletesiev']?></p>
-<h3>Könyveinek száma</h3>
-<p><?=$result['konyvekszama']?></p>
+    <h3>Író azonosítója</h3>
+    <p><?=$result['id']?></p>
+    <h3>Író neve</h3>
+    <p><?=$result['nev']?></p>
+    <h3>Születési éve</h3>
+    <p><?=$result['szuletesiev']?></p>
+    <h3>Könyveinek száma</h3>
+    <p><?=$result['konyvekszama']?></p>
 </div>
