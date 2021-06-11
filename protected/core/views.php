eab8dbfb5bb0788b3511caeef5d1487ba900818b
Konya Donat <konya.donat@gmail.com>
2021-06-11 02:20:24 +0200

    edited table view

--- protected/core/views.php
+++ protected/core/views.php
@@ -4,9 +4,9 @@
 function print_menu($menu, $return_html = false) {
 	$menuString = '';
 
-	$menuString .= '<ul id="menu">';
+	$menuString .= '<ul class="nav nav-tabs">';
 	foreach ($menu as &$menuItem) {
-		$menuString = $menuString.'<li class="parent"><a href="'.$menuItem['href'].'" '. (array_key_exists('extra',$menuItem) ? generate_attributes($menuItem['extra']) : '').'>'.$menuItem['title'].'</a>';
+		$menuString = $menuString.'<li class="nav-item"><a class="nav-link" href="'.$menuItem['href'].'" '. (array_key_exists('extra',$menuItem) ? generate_attributes($menuItem['extra']) : '').'>'.$menuItem['title'].'</a>';
 		if (array_key_exists('childs', $menuItem) && !empty($menuItem['childs'])){
 			print_submenu($menuItem['childs'], $menuString);
 		}
@@ -22,7 +22,7 @@ function print_menu($menu, $return_html = false) {
 }
 
 function print_submenu(&$menu, &$menuString){
-	$menuString .='<ul class ="child">';
+	$menuString .='<ul class ="nav nav-pills">';
 	$vangyermek = false;
 	foreach ($menu as &$menuItem) {
 		$vangyermek = false;
