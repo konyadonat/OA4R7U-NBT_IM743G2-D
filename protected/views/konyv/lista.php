0ea8656d87b20be135c4e22921ff7ed930887fa9
Konya Donat <konya.donat@gmail.com>
2021-06-11 18:54:06 +0200

    konyv fixed

--- protected/views/konyv/lista.php
+++ protected/views/konyv/lista.php
@@ -10,18 +10,18 @@
 <table class="table table-striped table-dark">
     <thead>
         <tr>
-            <th scope="col">Kiadó azonosítója</th>
-            <th scope="col">Író azonosítója </th>
-            <th scope="col">Könyvtár azonosítója</th>
             <th scope="col">Könyv címe</th>
             <th scope="col">Könyv témája</th>
             <th scope="col">Könyv kiadásának éve</th>
+            <th scope="col">Műveletek</th>
         </tr>
     </thead>
     <tbody>
         <?php foreach ($result as &$row): ?>
         <tr>
-            <td><?=$row['nev']?></td>
+            <td><?=$row['cim']?></td>
+            <td><?=$row['tema']?></td>
+            <td><?=$row['kiadaseve']?></td>
             <td>
                 <a href="<?=BASE_URL?>?E=konyv&M=megtekint&P=<?=$row['id']?>">Megtekint</a>
                 <a href="<?=BASE_URL?>?E=konyv&M=szerkeszt&P=<?=$row['id']?>">Szerkeszt</a>
