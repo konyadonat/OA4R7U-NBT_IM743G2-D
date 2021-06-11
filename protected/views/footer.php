4a4cd50a5fd79c76761898701a128bcc301aaeb2
Konya Donat <konya.donat@gmail.com>
2021-06-11 15:04:33 +0200

    footer edited

--- protected/views/footer.php
+++ protected/views/footer.php
@@ -6,7 +6,7 @@
   <!-- Copyright -->
   <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
     © 2021 Copyright:
-    <a class="text-white" href="https://github.com/konyadonat">Kónya Donát</a>
+    <a class="text-white" target="_blank" href="https://github.com/konyadonat">Kónya Donát</a>
   </div>
   <!-- Copyright -->
 </footer>
