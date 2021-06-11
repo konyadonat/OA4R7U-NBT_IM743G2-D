5dce3256d351bc8b6b2719c705968aa7edff6bc3
Konya Donat <konya.donat@gmail.com>
2021-06-11 18:16:51 +0200

    konyv view and content finished

---
+++ protected/views/konyv/lista.php
@@ -0,0 +1,37 @@
+<div class="container-fluid">
+<br/>
+<h1 class="text-center">Adatbázisban tárolt könyvek listája</h1>
+<br/>
+<h1 class="text-left"> <a href="<?=BASE_URL?>?E=konyv&M=hozzaad">Könyv hozzáadás</h1></a>
+
+<?php if($result == null || empty($result)): ?>
+<p>Nincs rögzítve rekord</p>
+<?php else : ?>
+<table class="table table-striped table-dark">
+    <thead>
+        <tr>
+            <th scope="col">Kiadó azonosítója</th>
+            <th scope="col">Író azonosítója </th>
+            <th scope="col">Könyvtár azonosítója</th>
+            <th scope="col">Könyv címe</th>
+            <th scope="col">Könyv témája</th>
+            <th scope="col">Könyv kiadásának éve</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($result as &$row): ?>
+        <tr>
+            <td><?=$row['nev']?></td>
+            <td>
+                <a href="<?=BASE_URL?>?E=konyv&M=megtekint&P=<?=$row['id']?>">Megtekint</a>
+                <a href="<?=BASE_URL?>?E=konyv&M=szerkeszt&P=<?=$row['id']?>">Szerkeszt</a>
+                <a href="<?=BASE_URL?>?E=konyv&M=torol&P=<?=$row['id']?>">Töröl</a>
+            </td>
+        </tr>
+        <?php endforeach; ?>
+    </tbody>    
+</table>
+<?php endif; ?>
+</div>
+
+
