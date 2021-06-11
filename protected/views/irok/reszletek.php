4587aea62b02d87294d762d8c418e6a6f74c953b
Konya Donat <konya.donat@gmail.com>
2021-06-11 18:53:36 +0200

    reszletek view added to irok

---
+++ protected/views/irok/reszletek.php
@@ -0,0 +1,8 @@
+<div class="container">
+<h3>Író neve</h3>
+<p><?=$result['nev']?></p>
+<h3>Születési éve</h3>
+<p><?=$result['szuletesiev']?></p>
+<h3>Könyveinek száma</h3>
+<p><?=$result['konyvekszama']?></p>
+</div>
