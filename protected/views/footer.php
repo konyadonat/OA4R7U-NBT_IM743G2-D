4071648bc4e3826a9e0488a067fb761048660fdc
Konya Donat <konya.donat@gmail.com>
2021-06-11 02:21:00 +0200

    Updated footer appearence

--- protected/views/footer.php
+++ protected/views/footer.php
@@ -1,4 +1,12 @@
-<hr>
-<center>
-&copy; 2021 | Kónya Donát | OA4R7U
-</center>
+<footer class="text-center text-white fixed-bottom" style="background-color: #21081a;">
+  <!-- Grid container -->
+  <div class="container p-4"></div>
+  <!-- Grid container -->
+
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2021 Copyright:
+    <a class="text-white" href="https://github.com/konyadonat">Kónya Donát</a>
+  </div>
+  <!-- Copyright -->
+</footer>
