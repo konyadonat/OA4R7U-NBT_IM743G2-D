3db4f231af6c2e0e8603e4c5fdf3ba6d54daefc1
Konya Donat <konya.donat@gmail.com>
2021-06-13 12:51:29 +0200

    kolcsonzo megtekint finished

---
+++ protected/views/kolcsonzo/reszletek.php
@@ -0,0 +1,12 @@
+<div class="container">
+    <h3>Kölcsönző azonosítója</h3>
+    <p><?=$result['id']?></p>
+    <h3>Kölcsönzött könyv azonosítója</h3>
+    <p><?=$result['konyvid']?></p>
+    <h3>Kölcsönző neve</h3>
+    <p><?=$result['nev']?></p>
+    <h3>Kölcsönző irányítószáma</h3>
+    <p><?=$result['iranyitoszam']?></p>
+    <h3>Kölcsönző címe</h3>
+    <p><?=$result['cim']?></p>
+</div>
