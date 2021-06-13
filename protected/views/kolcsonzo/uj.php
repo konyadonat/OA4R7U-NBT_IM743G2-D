f8018306f71bae67d322a0c7438e626e682d2897
Konya Donat <konya.donat@gmail.com>
2021-06-13 17:27:36 +0200

    kolcsonzo uj view done

---
+++ protected/views/kolcsonzo/uj.php
@@ -0,0 +1,23 @@
+</br>
+<div class="container">
+<h2>Kölcsönző rögzítése</h2>
+<form action="" method="POST" accept-charset="UTF-8">
+        <div class="form-group">
+            <label for="konyvid">Könyv azonosítója: </label>
+            <input class="form-control" type="text" name="konyvid" required="required" id="konyvid" /> <br/>
+        </div>
+        <div class="form-group">
+            <label for="nev">Kölcsöznő neve: </label>
+            <input class="form-control" type="nev" name="szuletesiev" required="required" id="nev" minlength="6" maxlength="40"/> <br/>
+        </div>
+        <div class="form-group">
+            <label for="iranyitoszam">Kölcsönző irányítószáma: </label>
+            <input type="text" class="form-control" name="iranyitoszam" required="required" id="iranyitoszam" maxlength="4"/> <br/>
+        </div>
+        <div class="form-group">
+            <label for="cim">Kölcsönző címe: </label>
+            <input type="text" class="form-control" name="cim" required="required" id="cim" maxlength="40"/> <br/>
+        </div>
+        <button type="submit" name="submit" value="1">Mentés </button>
+    </form>
+</div>
