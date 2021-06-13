5beec37efbf6254cd28afc04991890c4d91bf100
Konya Donat <konya.donat@gmail.com>
2021-06-13 13:00:18 +0200

    konyvtar megtekint finished

---
+++ protected/views/konyvtar/reszletek.php
@@ -0,0 +1,11 @@
+<div class="container">
+    <h3>Könyvtár azonosítója</h3>
+    <p><?=$result['id']?></p>
+    <h3>Könyvtár neve</h3>
+    <p><?=$result['nev']?></p>
+    <h3>Könyvtár irányítószáma</h3>
+    <p><?=$result['iranyitoszam']?></p>
+    <h3>Könyvtár címe</h3>
+    <p><?=$result['cim']?></p>
+</div>
+
