7038420b6ee0358dd4fa8ec969c73c64f6897e85
Konya Donat <konya.donat@gmail.com>
2021-06-13 12:59:57 +0200

    konyv megtekint finished

---
+++ protected/views/konyv/reszletek.php
@@ -0,0 +1,16 @@
+<div class="container">
+    <h3>Könyv azonosítója</h3>
+    <p><?=$result['id']?></p>
+    <h3>Kiadó azonosítója</h3>
+    <p><?=$result['kiadoid']?></p>
+    <h3>Író azonosítója</h3>
+    <p><?=$result['iroid']?></p>
+    <h3>Könyvtár azonosítója</h3>
+    <p><?=$result['konyvtarid']?></p>
+    <h3>Könyv címe</h3>
+    <p><?=$result['cim']?></p>
+    <h3>Könyv témája</h3>
+    <p><?=$result['tema']?></p>
+    <h3>Könyv kiadásának éve</h3>
+    <p><?=$result['kiadaseve']?></p>
+</div>
