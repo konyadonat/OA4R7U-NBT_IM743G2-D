5b3b4f571faabad81ac11618574569af05567008
Konya Donat <konya.donat@gmail.com>
2021-06-13 18:27:20 +0200

    string validator added

--- protected/content/kolcsonzo.php
+++ protected/content/kolcsonzo.php
@@ -98,7 +98,7 @@ function hozzaad(){
     else
     {
         $konyvid = filter_input(INPUT_POST,'konyvid',FILTER_SANITIZE_NUMBER_INT);
-        $nev = filter_input(INPUT_POST,'nev');
+        $nev = filter_input(INPUT_POST,'nev',FILTER_SANITIZE_STRING);
         $iranyitoszam = filter_input(INPUT_POST,'iranyitoszam',FILTER_SANITIZE_NUMBER_INT);
         $cim = filter_input(INPUT_POST,'cim',FILTER_SANITIZE_STRING);
 
