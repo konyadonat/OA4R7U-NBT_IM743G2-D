4f51895f21da996c923728b9e8f469756468fffd
Konya Donat <konya.donat@gmail.com>
2021-06-13 15:12:46 +0200

    szerkeszt view added

---
+++ protected/views/irok/szerkeszt.php
@@ -0,0 +1,15 @@
+</br>
+<div class="container">
+<h2>Író szerkesztése</h2>
+<form action="" method="POST" accept-charset="UTF-8">
+        <div class="form-group">
+            <label for="nev">Író neve: </label>
+            <input class="form-control" type="text" name="nev" required="required" id="nev" minlength="6" maxlength="30"/> <br/>
+        </div>
+        <div class="form-group">
+            <label for="konyvekszama">Kiadott könyvek száma: </label>
+            <input type="text" class="form-control" name="konyvekszama" required="required" id="konyvekszama" maxlength="4"/> <br/>
+        </div>
+        <button type="submit" name="submit" value="1">Mentés </button>
+    </form>
+</div>
