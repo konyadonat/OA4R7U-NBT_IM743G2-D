21501fe28296c7f640ae6e281719c33a63b50cf8
Konya Donat <konya.donat@gmail.com>
2021-06-13 12:47:33 +0200

    added functions to konyv

--- protected/content/konyv.php
+++ protected/content/konyv.php
@@ -8,4 +8,26 @@ function lista(){
     $result = select($query);
     require_once VIEWS_DIR.'konyv/lista.php';
 }
+function get_p(){
+    if(!filter_has_var(INPUT_GET, 'P')){
+        die('Helytelen paraméter!');
+    }
+    $id = filter_input(INPUT_GET, 'P', FILTER_VALIDATE_INT);
+    if($id === false){
+        die('Helytelen paraméter!');
+    }
+    return $id;
+}
+
+function megtekint(){
+    $id = get_p();
+    
+    $result = select('SELECT * FROM konyv WHERE id =:id',TRUE, ['id' => $id]);
+    
+    if($result === false){
+        die('Nincs ilyen rekord!');
+    }
+    
+    require_once VIEWS_DIR.'konyv/reszletek.php';
+}
 
