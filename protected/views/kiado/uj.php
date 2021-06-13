f16d865ca5c3300bfd901374ecee8b9d664d8d1c
Konya Donat <konya.donat@gmail.com>
2021-06-13 18:27:32 +0200

    kiado uj view created

---
+++ protected/views/kiado/uj.php
@@ -0,0 +1,11 @@
+</br>
+<div class="container">
+<h2>Író rögzítése</h2>
+<form action="" method="POST" accept-charset="UTF-8">
+        <div class="form-group">
+            <label for="nev">Kiadó neve: </label>
+            <input class="form-control" type="text" name="nev" required="required" id="nev" minlength="6" maxlength="30"/> <br/>
+        </div>
+        <button type="submit" name="submit" value="1">Mentés </button>
+    </form>
+</div>
